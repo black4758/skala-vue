a4b15d779f290ad9018412aeed170299b085bfa7
black4758 <black475867@gmail.com>
2026-08-13 15:33:45 +0900

    style: 단위 변경 버튼 클릭 후 포커스 잔상 남는 UI 버그 수정

--- src/components/exercise/UnitToggler.vue
+++ src/components/exercise/UnitToggler.vue
@@ -4,11 +4,19 @@ import { useConfigStore } from '../../stores/configStore'
 import Button from 'primevue/button'
 
 const configStore = useConfigStore()
+
+const handleToggle = (event) => {
+  configStore.toggleUnit()
+  // 버튼 클릭 후 포커스(눌림 상태)를 강제로 해제합니다.
+  if (event.currentTarget) {
+    event.currentTarget.blur()
+  }
+}
 </script>
 
 <template>
   <Button 
-    @click="configStore.toggleUnit"
+    @click="handleToggle"
     :label="`단위: ${configStore.unitSymbol}`"
     icon="pi pi-sync"
     severity="secondary"
